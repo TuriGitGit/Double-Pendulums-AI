175f022a1fe24be768bca75416e5cf0c7bc4446b
TuriGitGit <161383199+TuriGitGit@users.noreply.github.com>
2025-08-31 08:53:11 -0400

    Update CSVtoBinary.py

--- CSVtoBinary.py
+++ CSVtoBinary.py
@@ -1,3 +1,3 @@
 import numpy as np
-data = np.loadtxt("200000000 simulations at 0.00235 for 2.00 using 1.csv", delimiter=",", skiprows=1, dtype=np.float32) # using dtype=np.float32 is suggested BC CURRENT PYTORCH DEFAULTS TO 64 >:[
-np.save("chunk200m1.npy", data)
+data = np.loadtxt("500000000 simulations at 0.00333 using 1.csv", delimiter=",", skiprows=1, dtype=np.float32) # using dtype=np.float32 is suggested BC CURRENT PYTORCH DEFAULTS TO 64 >:[
+np.save("chunk500m1.npy", data)
