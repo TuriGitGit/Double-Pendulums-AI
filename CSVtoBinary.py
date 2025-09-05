3ed1e51d10ec21db49947c8cff7f092e09824d07
Westin Smith <161383199+TuriGitGit@users.noreply.github.com>
2025-09-04 20:39:17 -0400

    Update CSVtoBinary.py
    future Evalulate.py compatibility

--- CSVtoBinary.py
+++ CSVtoBinary.py
@@ -1,3 +1,17 @@
 import numpy as np
-data = np.loadtxt("150000000 simulations at 0.00333 using 1.csv", delimiter=",", skiprows=1, dtype=np.float32) # using dtype=np.float32 is suggested BC CURRENT PYTORCH DEFAULTS TO 64 >:[
-np.save("chunk150m1.npy", data)
+data = np.loadtxt("150000000 simulations at 0.00333 using 1.csv", delimiter=",", skiprows=1, dtype=np.float32)
+
+input_mean = data[:, :7].mean(axis=0, dtype=np.float32)
+input_std = data[:, :7].std(axis=0, dtype=np.float32)
+target_mean = data[:, 7:].mean(axis=0, dtype=np.float32)
+target_std = data[:, 7:].std(axis=0, dtype=np.float32)
+
+try:
+    np.savez("150m stats.npz", input_mean=input_mean, input_std=input_std, target_mean=target_mean, target_std=target_std)
+except Exception as e:
+    print("failed to save stats.npz", e)
+
+try:
+    np.save("150m data.npy", data)
+except Exception as e:
+    print("failed to save data")
